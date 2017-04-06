a5b41f338dbfef7a8489e213b9858ab66c8c0eed
YuriPopiv <popiv.yuri@gmail.com>
2017-04-06 13:49:52 +0300

    updates

---
+++ app/src/main/java/com/example/yura9/testteamvoy/model/User.java
@@ -0,0 +1,125 @@
+package com.example.yura9.testteamvoy.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by yura9 on 4/6/2017.
+ */
+
+public class User {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("portfolio_url")
+    @Expose
+    private String portfolioUrl;
+    @SerializedName("bio")
+    @Expose
+    private String bio;
+    @SerializedName("location")
+    @Expose
+    private String location;
+    @SerializedName("total_likes")
+    @Expose
+    private Integer totalLikes;
+    @SerializedName("total_photos")
+    @Expose
+    private Integer totalPhotos;
+    @SerializedName("total_collections")
+    @Expose
+    private Integer totalCollections;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPortfolioUrl() {
+        return portfolioUrl;
+    }
+
+    public void setPortfolioUrl(String portfolioUrl) {
+        this.portfolioUrl = portfolioUrl;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getTotalLikes() {
+        return totalLikes;
+    }
+
+    public void setTotalLikes(Integer totalLikes) {
+        this.totalLikes = totalLikes;
+    }
+
+    public Integer getTotalPhotos() {
+        return totalPhotos;
+    }
+
+    public void setTotalPhotos(Integer totalPhotos) {
+        this.totalPhotos = totalPhotos;
+    }
+
+    public Integer getTotalCollections() {
+        return totalCollections;
+    }
+
+    public void setTotalCollections(Integer totalCollections) {
+        this.totalCollections = totalCollections;
+    }
+
+
+
+}
